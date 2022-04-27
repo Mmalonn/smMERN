b63660876a5ed41ef7c90c394713200badece37d
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-27 17:16:42 -0300

    conectado a la base de datos

--- server/index.js
+++ server/index.js
@@ -2,3 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const MONGODB_URI = process.env.URI;
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`server running on port: ${PORT}`);
+    })
+  )
+  .catch((error) => {
+    console.log(error);
+  });
