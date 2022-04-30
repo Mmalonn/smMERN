c0bcd771ae77d990e68197fc4341aa9cc28d7bf8
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-30 09:37:28 -0300

    creada funcion para actualizar el post en el backend

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPost = async (req, res) => {
@@ -19,4 +20,15 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("no post with that id");
+  const updatedPost = awaitPostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+  res.json(updatePost);
 };
--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createPost, getPost } from "../controllers/posts.controllers.js";
+import { createPost, getPost, updatePost } from "../controllers/posts.controllers.js";
 const router = Router();
 
 router.get("/posts", getPost);
 router.post("/posts", createPost);
+router.patch("/posts/:id", updatePost)
 
 export default router;
