6eb670999ee3f58f337133c2272f0cd9b50ec61b
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-30 18:25:20 -0300

    creada funcion de borrar en el backend

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createPost, getPost, updatePost } from "../controllers/posts.controllers.js";
+import { createPost, getPost, updatePost, deletePost } from "../controllers/posts.controllers.js";
 const router = Router();
 
 router.get("/posts", getPost);
 router.post("/posts", createPost);
-router.patch("/posts/:id", updatePost)
+router.patch("/posts/:id", updatePost);
+router.delete("/posts/:id", deletePost);
 
 export default router;
