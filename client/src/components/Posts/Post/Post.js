7cd904f61464babb7639abb57aa6da1a78ec22f1
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-30 11:01:47 -0300

    creada el pedido de udpate de la carpeta api, creada la accion para ejecutar esa funcion, ajustado el reducer, y puesto en el boton para guardar del form

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -13,7 +13,7 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
@@ -25,11 +25,15 @@ const Post = ({ post }) => {
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
         <Typography variant="body2">
-          {moment(post.createdAt).fromNow()}    
+          {moment(post.createdAt).fromNow()}
         </Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+        <Button
+          style={{ color: "white" }}
+          size="small"
+          onClick={() => setCurrentId(post._id)}
+        >
           <MoreHorizIcon fontSize="medium" />
         </Button>
       </div>
@@ -38,19 +42,22 @@ const Post = ({ post }) => {
           {post.tags.map((tag) => `#${tag} `)}
         </Typography>
       </div>
+      <Typography className={classes.title} variant="h5" gutterBottom>
+        {post.title}
+      </Typography>
       <CardContent>
-        <Typography className={classes.title} variant="h5" gutterBottom>
+        <Typography variant="h5" gutterBottom>
           {post.message}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={()=>{}}>
-          <ThumbUpAltIcon fontSize="small"/>
-          Like 
+        <Button size="small" color="primary" onClick={() => {}}>
+          <ThumbUpAltIcon fontSize="small" />
+          Like
           {` ${post.likeCount}`}
         </Button>
-        <Button size="small" color="primary" onClick={()=>{}}>
-          <DeleteIcon fontSize="small"/>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <DeleteIcon fontSize="small" />
           Delete
         </Button>
       </CardActions>
--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -27,8 +27,12 @@ export const updatePost = async (req, res) => {
   const post = req.body;
   if (!mongoose.Types.ObjectId.isValid(_id))
     return res.status(404).send("no post with that id");
-  const updatedPost = awaitPostMessage.findByIdAndUpdate(_id, post, {
-    new: true,
-  });
-  res.json(updatePost);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  );
+  res.json(updatedPost);
 };
