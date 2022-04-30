ac309f4fb002d60e4f36db056cca5b1fc354bc79
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-30 18:40:49 -0300

    creada funcion delete en api para conectarla con el backend, creada en el actions, configurada en el reducer y colocada con dispatch en su boton del post

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -39,7 +39,7 @@ export const updatePost = async (req, res) => {
 
 export const deletePost = async (req, res) => {
   const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(_id))
+  if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("no post with that id");
   await PostMessage.findByIdAndDelete(id);
   res.json({ message: "Post deleted successfully" });
