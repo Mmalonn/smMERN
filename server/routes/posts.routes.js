6db40aed6b2a454611f6c6283941928c760cbb87
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-30 23:30:58 -0300

    creada funcion en el backend para cambiar elestado de likecount

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createPost, getPost, updatePost, deletePost } from "../controllers/posts.controllers.js";
+import { createPost, getPost, updatePost, deletePost,likePost } from "../controllers/posts.controllers.js";
 const router = Router();
 
 router.get("/posts", getPost);
 router.post("/posts", createPost);
 router.patch("/posts/:id", updatePost);
 router.delete("/posts/:id", deletePost);
+router.patch("/posts/:id/likePost", likePost)
 
 export default router;
