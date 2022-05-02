353cd96189ad39d75c2c6778920014fe95295fd9
marcos torres <marcosezequiel_torres@hotmail.com>
2022-05-02 10:51:39 -0300

    creado componente de home para no tener elementos sueltos en el app.js, solo componentes importados

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import React from "react";
+import { Container } from "@material-ui/core";
 import { BrowserRouter, Swich, Route } from "react-router-dom";
 
-import { getPosts } from "./actions/posts";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import useStyles from "./styles";
 import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home"
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
         <NavBar />
-        <Grow in>
-          <Container>
-            <Grid
-              container
-              className={classes.mainContainer}
-              justifyContent="space-between"
-              alignItems="stretch"
-              spacing={3}
-            >
-              <Grid item xs={12} sm={7}>
-                <Posts setCurrentId={setCurrentId} />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <Form currentId={currentId} setCurrentId={setCurrentId} />
-              </Grid>
-            </Grid>
-          </Container>
-        </Grow>
+        <Home />
       </Container>
     </BrowserRouter>
   );
