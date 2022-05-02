ba1ff642d7ecdff027b94140a60c5644e710e858
marcos torres <marcosezequiel_torres@hotmail.com>
2022-05-02 10:16:55 -0300

    problema solucionado, la etiqueta link no se puede usar fuera de un componente router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Container, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
+import { BrowserRouter, Swich, Route } from "react-router-dom";
 
 import { getPosts } from "./actions/posts";
 import Posts from "./components/Posts/Posts";
@@ -18,27 +19,29 @@ const App = () => {
   }, [currentId, dispatch]);
 
   return (
-    <Container maxWidth="lg">
-      <NavBar/>
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            className={classes.mainContainer}
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <NavBar />
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              className={classes.mainContainer}
+              justifyContent="space-between"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid item xs={12} sm={7}>
+                <Posts setCurrentId={setCurrentId} />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+          </Container>
+        </Grow>
+      </Container>
+    </BrowserRouter>
   );
 };
 
