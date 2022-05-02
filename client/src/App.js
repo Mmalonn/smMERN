459df09c918e8fdabf02d9614b05137ea1f029d3
marcos torres <marcosezequiel_torres@hotmail.com>
2022-05-02 11:41:22 -0300

    agregando cosas a pagina de autenticacion

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Swich, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar/NavBar";
-import Home from "./components/Home/Home"
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
         <NavBar />
-        <Home />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
       </Container>
     </BrowserRouter>
   );
