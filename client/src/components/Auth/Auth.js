09acb0ca4885474e05026b83b316b72ce1313703
marcos torres <marcosezequiel_torres@hotmail.com>
2022-05-03 11:02:00 -0300

    creado campo para ingresar el nombre en el login

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -6,6 +6,7 @@ import {
   Grid,
   Typography,
   Container,
+  TextField,
 } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOpenOutlined";
 import useStyles from "./styles";
@@ -13,6 +14,11 @@ import useStyles from "./styles";
 const Auth = () => {
   const classes = useStyles();
   const isSignup = false;
+
+  const handleSubmit = () => {};
+
+  const handleChange = () => {};
+
   return (
     <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
@@ -20,7 +26,21 @@ const Auth = () => {
           <LockOutlinedIcon />
         </Avatar>
         <Typography variant="h5">{isSignup ? "Sign Up" : "Sign in"}</Typography>
-        
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <TextField
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  xs={6}
+                />
+              </>
+            )}
+          </Grid>
+        </form>
       </Paper>
     </Container>
   );
