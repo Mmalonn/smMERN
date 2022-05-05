9ece54957058dcc53f1c322d8d825d6a525c4b60
marcos torres <marcosezequiel_torres@hotmail.com>
2022-05-05 20:10:46 -0300

    creada la funcionalidad del boton de logout

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -3,18 +3,25 @@ import { Link } from "react-router-dom";
 import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
-
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const NavBar = () => {
   const classes = useStyles();
-  const [user, setUser]= useState(JSON.parse(localStorage.getItem("profile")));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  console.log(user);
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    history.push("/auth");
+    setUser(null);
+  };
 
-  useEffect(()=>{
-    const token = user?.token;
-    setUser(JSON.parse(localStorage.getItem("profile")))
-  },[]);
+  // useEffect(() => {
+  //   const token = user?.token;
+  //   setUser(JSON.parse(localStorage.getItem("profile")));
+  // }, []);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -52,6 +59,7 @@ const NavBar = () => {
               variant="contained"
               className={classes.logout}
               color="secondary"
+              onClick={logout}
             >
               Logout
             </Button>
