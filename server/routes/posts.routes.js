7e8d4205edf3eb160bc51618026d3d8d49377dcb
marcos torres <marcosezequiel_torres@hotmail.com>
2022-05-05 18:35:53 -0300

    volvi a un commit anterior porque habia un problema del cual no encontraba solucion y agregue /posts/ a las rutas para trabajar con los posts

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { createPost, getPost, updatePost, deletePost,likePost } from "../controllers/posts.controllers.js";
 const router = Router();
 
-router.get("/", getPost);
-router.post("/", createPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost)
+router.get("/posts", getPost);
+router.post("/posts", createPost);
+router.patch("/posts/:id", updatePost);
+router.delete("/posts/:id", deletePost);
+router.patch("/posts/:id/likePost", likePost)
 
 export default router;
