6f9ce2b0506bb9b7a46ea752d1c13f9cdd7d81d4
marcos torres <marcosezequiel_torres@hotmail.com>
2022-05-06 07:31:13 -0300

    creadas funcoines de signin y signup en el backend, hasta ahora todo bien

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -4,7 +4,7 @@ import PostMessage from "../models/postMessage.js";
 export const getPost = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-    console.log(postMessages);
+    console.log("posts enviados");
     res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({ error: message });
