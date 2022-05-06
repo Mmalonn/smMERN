44763fa5ce806afa33d277d85e02e6d922e0af76
marcos torres <marcosezequiel_torres@hotmail.com>
2022-05-06 08:39:57 -0300

    modificada la funcion de sumar likes en el backend

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { createPost, getPost, updatePost, deletePost,likePost } from "../controllers/posts.controllers.js";
+import {
+  createPost,
+  getPost,
+  updatePost,
+  deletePost,
+  likePost,
+} from "../controllers/posts.controllers.js";
 const router = Router();
+import auth from "../middleware/auth.js";
 
 router.get("/posts", getPost);
-router.post("/posts", createPost);
-router.patch("/posts/:id", updatePost);
-router.delete("/posts/:id", deletePost);
-router.patch("/posts/:id/likePost", likePost)
+router.post("/posts", auth, createPost);
+router.patch("/posts/:id", auth, updatePost);
+router.delete("/posts/:id", auth, deletePost);
+router.patch("/posts/:id/likePost", auth, likePost);
 
 export default router;
